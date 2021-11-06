16a3b70fcb080fa35fe9b1870101b3605eb1ff15
Kaspar Martin Suursalu <kasparsu@gmail.com>
2021-11-06 13:44:13 +0200

    add db and laravel

--- docker/web.dockerfile
+++ docker/web.dockerfile
@@ -1,5 +1,10 @@
-FROM php:apache
+FROM php:8.1-rc-apache
 
 RUN apt-get update && apt-get install cowsay -y
 
-RUN PATH="$PATH:/usr/games" && export PATH
+ENV PATH="/usr/games:${PATH}"
+
+COPY vhost.conf /etc/apache2/sites-enabled/000-default.conf
+
+RUN a2enmod rewrite
+RUN docker-php-ext-install pdo pdo_mysql
---
+++ html/app/Http/Controllers/PublicController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PublicController extends Controller
+{
+    public function home(){
+        $name = "Kaspar";
+        $array = ['piim', 'sai', 'leib'];
+        return view('home', compact('name', 'array'));
+    }
+
+    public function posts(){
+        //$posts = Post::select(['title', 'id'])->where('title','LIKE', '%et %')->where('id', '<', 500)->limit(10)->orderBy('title', 'desc')->get();
+
+        $posts = Post::simplePaginate(16);
+        return view('posts', compact('posts'));
+    }
+}
